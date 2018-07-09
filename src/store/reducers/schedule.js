17243a6d9597d78a303e91c66eb9ffdc34cdb556
IwYvI <fh635020058@hotmail.com>
2018-07-09 14:01:51 +0800

    加入日程页面

--- src/store/reducers/schedule.js
+++ src/store/reducers/schedule.js
@@ -25,11 +25,15 @@ export default handleActions({
     payload
   }) {
     const list = payload.map((v) => v.attributes)
+    const now = Date.now()
     state.regular.list.push(...list.filter((v) => v.mode_key === 'regular' && v.end_time > state.regular.lastEndTime))
+    state.regular.list = state.regular.list.filter((v) => parseInt(`${v.end_time}000`) > now)
     state.regular.lastEndTime = state.regular.list[state.regular.list.length - 1].end_time
     state.gachi.list.push(...list.filter((v) => v.mode_key === 'gachi' && v.end_time > state.gachi.lastEndTime))
+    state.gachi.list = state.gachi.list.filter((v) => parseInt(`${v.end_time}000`) > now)
     state.gachi.lastEndTime = state.gachi.list[state.gachi.list.length - 1].end_time
     state.league.list.push(...list.filter((v) => v.mode_key === 'league' && v.end_time > state.league.lastEndTime))
+    state.league.list = state.league.list.filter((v) => parseInt(`${v.end_time}000`) > now)
     state.league.lastEndTime = state.league.list[state.league.list.length - 1].end_time
     return {
       ...state
